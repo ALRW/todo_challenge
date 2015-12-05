ce8f4a41f9b2585bd44d9d796e0a650e721e5cce
ALRW <awerner1@googlemail.com>
2015-12-05 13:35:04 +0000

    Added base ListController

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-var toDoListPage = angular.module('ToDoList', ['ngResource']);
+(function(){
+  var toDoListPage = angular.module('ToDoList', ['ngResource']);
+
+  toDoListPage.controller("ListController", function(){
+    this.taskList = task;
+  });
+
+  var task = {
+    item: "Pay Bills",
+    status: 'incomplete'
+  }
+})();
--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,7 +1,16 @@
-describe('To Do List', function(){
-  it('has a title', function(){
+describe('To Do List: ', function(){
+  beforeEach(function(){
     browser.get('http://127.0.0.1:8080');
+  });
 
+  it('Loads the homepage', function(){
     expect(browser.getTitle()).toEqual("To do List");
   });
+
+  it('can store a item in the to do list', function(){
+    element(by.model('toDoCtrl.toDoTerm')).sendKeys('Pay Bills');
+    element(by.id('Add')).click();
+    expect(element(by.binding("list.item")).getText()).toEqual('Pay Bills')
+  });
+
 });
